5bde8def8ca2a09505ea5caf57a1548b72969233
erdem koyuncu <erdemfb1907@outlook.com>
2024-10-07 19:37:46 +0300

    Some lil updates

--- quantumClubWebsite/frontend/src/components/ui/roadmap/checkpoint1/check1Body.jsx
+++ quantumClubWebsite/frontend/src/components/ui/roadmap/checkpoint1/check1Body.jsx
@@ -4,6 +4,12 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { MonitorPlay, Clapperboard } from 'lucide-react'
 import 'swiper/css'
 import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import {
   Drawer,
   DrawerClose,
   DrawerContent,
@@ -82,21 +88,36 @@ const check1Body = () => {
         transition={{ ease: 'easeOut', duration: 0.6 }}
         className="rounded-2xl border bg-neutral-100 shadow-lg dark:bg-neutral-900"
       >
-        <div className="px-4 py-3">
-          <div className="flex flex-row items-center gap-2 pb-1">
-            <MonitorPlay />
-            <h1 className="text-xl font-semibold">Dizi önerileri</h1>
+        <div className="py-3">
+          <div className="px-4">
+            <Accordion collapsible className="w-full">
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="py-3">
+                  <h1 className="flex flex-row items-center gap-2 text-left text-xl font-semibold">
+                    <MonitorPlay />
+                    Dizi önerileri
+                  </h1>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <p className="text-sm text-neutral-400">
+                    Not: Daha iyi bir öğrenme için aşağıdaki önerilerden en az
+                    birini izlemeniz gerekmektedir.
+                  </p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
-          <p className="text-sm text-neutral-400">
-            Not: Daha iyi bir öğrenme için aşağıdaki önerilerden en az birini
-            izlemeniz gerekmektedir.
-          </p>
-          <Swiper spaceBetween={10} slidesPerView={'auto'} className="my-2">
+
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={'auto'}
+            className="my-2 px-2"
+          >
             {seriesContent.map((content, index) => (
               <SwiperSlide key={index} className="w-auto rounded-lg">
                 <Drawer>
                   <DrawerTrigger>
-                    <div className="h-36 w-24 overflow-hidden rounded-md bg-neutral-700">
+                    <div className="h-40 w-[6.75rem] overflow-hidden rounded-md bg-neutral-700">
                       <img
                         src={content.image}
                         alt=""
@@ -129,20 +150,34 @@ const check1Body = () => {
             ))}
           </Swiper>
 
-          <div className="flex flex-row items-center gap-2 pb-1 pt-4">
-            <Clapperboard />
-            <h1 className="text-xl font-semibold">Film önerileri</h1>
+          <div className="px-4">
+            <Accordion collapsible className="w-full">
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="py-3">
+                  <h1 className="flex flex-row items-center gap-2 text-left text-xl font-semibold">
+                    <MonitorPlay />
+                    Film önerileri
+                  </h1>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <p className="text-sm text-neutral-400">
+                    Not: Daha iyi bir öğrenme için aşağıdaki önerilerden en az
+                    birini izlemeniz gerekmektedir.
+                  </p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
-          <p className="text-sm text-neutral-400">
-            Not: Daha iyi bir öğrenme için aşağıdaki önerilerden en az birini
-            izlemeniz gerekmektedir.
-          </p>
-          <Swiper spaceBetween={10} slidesPerView={'auto'} className="my-2">
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={'auto'}
+            className="my-2 px-2"
+          >
             {movieContent.map((content, index) => (
               <SwiperSlide key={index} className="w-auto rounded-lg">
                 <Drawer>
                   <DrawerTrigger>
-                    <div className="h-36 w-24 overflow-hidden rounded-md bg-neutral-700">
+                    <div className="h-40 w-[6.75rem] overflow-hidden rounded-md bg-neutral-700">
                       <img
                         src={content.image}
                         alt=""
